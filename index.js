873c324cd4110ccc5dae5622dc91a2b0e9edfd9d
Jose Raphael Mercado <98337482+raphmercado@users.noreply.github.com>
2023-05-13 20:18:21 +0800

    Move inline CSS styles to external stylesheet

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 function printGrid() {
   const container = document.querySelector(".container");
-  for (let count = 0; count < 16; count++) {
+  container.className = "container";
+  for (let count = 1; count <= 16; count++) {
     for (let i = 1; i <= 16; i++) {
       const grid = document.createElement("div");
       grid.setAttribute("id", "grid");
-      container.style.backgroundColor = "white";
-      container.style.display = "flex";
-      container.style.padding = " 0px 415px";
-      container.style.alignContent = "center";
-      container.style.justifyContent = "center";
-      container.style.flexWrap = "wrap";
-      container.style.height = "100vh";
-      grid.style.backgroundColor = "lightgrey";
-      grid.style.width = "5vh";
-      grid.style.height = "5vh";
-      grid.style.border = "1px solid white";
+      grid.className = "grid";
       container.appendChild(grid);
     }
   }
